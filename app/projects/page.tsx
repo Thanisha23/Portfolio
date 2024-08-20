ecec2e5d3b81c243c00e2ed940b93dede7041c7f
Thanisha Belchada <thanishab77@gmail.com>
2024-08-20 13:55:49 +0530

    fix: some changes related to dark mode

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -51,7 +51,7 @@ const ProjectsPage = () => {
               <CardBody className="relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black bg-black dark:border-white/[0.2] border-white/[0.2] w-[22rem] sm:w-[30rem] h-[29rem] md:h-[29rem] rounded-xl p-6 border">
                 <CardItem
                   translateZ="50"
-                  className="text-2xl font-bold text-neutral-600 dark:text-white mx-auto"
+                  className="text-2xl font-bold text-white dark:text-white mx-auto"
                 >
                   {item.name}
                 </CardItem>
@@ -62,7 +62,7 @@ const ProjectsPage = () => {
                 <CardItem
                   as="p"
                   translateZ="60"
-                  className="md:text-neutral-500 text-neutral-500 md:text-sm text-sm md:max-w-sm max-w-sm md:h-10 h-10 md:overflow-hidden overflow-hidden md:mt-3 mt-3 md:dark:text-neutral-300 dark:text-neutral-300 md:mx-auto mx-auto"
+                  className="md:text-neutral-500 text-neutral-300 md:text-sm text-sm md:max-w-sm max-w-sm md:h-10 h-10 md:overflow-hidden overflow-hidden md:mt-3 mt-3 md:dark:text-neutral-300 dark:text-neutral-300 md:mx-auto mx-auto"
                 >
                   {item.about}
                 </CardItem>
@@ -80,7 +80,7 @@ const ProjectsPage = () => {
                 <CardItem
                   translateZ={20}
                   as="button"
-                  className="px-4 flex gap-4 justify-center items center py-2 rounded-xl text-xs font-normal dark:text-white md:mt-2 mt-2"
+                  className="px-4 flex gap-4 justify-center items center py-2 rounded-xl text-xs font-normal text-white dark:text-white md:mt-2 mt-2"
                 >
                   {item.tech_stack.map((tech, techIndex) => (
                     <Image
@@ -110,7 +110,7 @@ const ProjectsPage = () => {
                   <CardItem
                     translateZ={20}
                     as="button"
-                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white text-white"
                   >
                     <a href={item.link}>Check Live →</a>
                   </CardItem>
