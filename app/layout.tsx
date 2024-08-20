0c8d549ee85270ef7b3b9a06b2bdcf1b81180e69
Thanisha Belchada <thanishab77@gmail.com>
2024-08-20 12:26:46 +0530

    fix: some changes related to dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
    
-    <html lang="en">
+    <html className='dark' lang="en">
       <body className={`${inter.className}`}>
       <Toaster />
       <ThemeProvider
