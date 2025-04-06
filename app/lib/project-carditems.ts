ff123fc45c0447d3bb65653b15fc82ebdba887a6
Thanisha Belchada <thanishab77@gmail.com>
2025-04-07 00:06:46 +0530

    feat: enhance project card items structure and styling; add custom scrollbar; update images

--- app/lib/project-carditems.ts
+++ app/lib/project-carditems.ts
@@ -1,149 +1,386 @@
 interface Items {
-    name: string,
-    tech_stack:string[],
-    status:string,
-    about : string,
-    image:string,
-    link:string,
-    repo:string,
+  name: string;
+  tech_stack: {
+    name: string;
+    badge: string;
+  }[];
+  status: string;
+  about: string;
+  image: string;
+  link: string;
+  repo: string;
 }
 
 const items: Items[] = [
-    {
-        name:"EonCapsule",
-        tech_stack:[
-            "/logos/next-js-white.svg",
-            "logos/react-2.svg",
-            "/logos/ts-logo-128.svg",
-            "/logos/tailwind-css-2.svg",
-            "/logos/postgreSQL.png",
-            "/logos/prisma.png",
-            "/logos/express-1.png"
-        ],
-        status:"Ongoing",
-        about:"Virtual Time Capsule lets users store and schedule digital messages, files, and videos for future release.",
-        image:"eoncapsule.png",
-        link:"",
-        repo:"",
-    },
-    {
-        name:"Blogosphere",
-        tech_stack:[
-            "logos/react-2.svg",
-            "/logos/ts-logo-128.svg",
-            "/logos/tailwind-css-2.svg",
-            "/logos/hono-1.png",
-            "/logos/postgreSQL.png",
-            "/logos/prisma.png",
-            "/logos/cloudflare-Workers.png"
-        ],
-        status:"Completed",
-        about:"Create, manage, share content effortlessly with a responsive, user-friendly design.",
-        image:"blogospheree.jpeg",
-        link:"https://blogospheree.vercel.app/",
-        repo:"https://github.com/Thanisha23/Blogosphere",
-    },
-    {
-        name:"PayBuddy",
-        tech_stack:[
-            "logos/react-2.svg",
-            "/logos/ts-logo-128.svg",
-            "/logos/tailwind-css-2.svg",
-            "/logos/mongodb.png",
-            "/logos/express-1.png",
-            "/logos/nodejs-1.svg"
-        ],
-        status:"Completed",
-        about:"A responsive banking application with seamless  UI and transaction capabilities, styled with Tailwind",
-        image:"paybuddy-1.png",
-        link:"https://pay-buddy-lilac.vercel.app/",
-        repo:"https://github.com/Thanisha23/PayBuddy",
-    },
-    {
-        name:"My Portfolio",
-        tech_stack:[
-            "/logos/next-js-white.svg",
-            "/logos/ts-logo-128.svg",
-            "/logos/tailwind-css-2.svg",
-        ],
-        status:"Completed",
-        about:"The very place where your cursor is currently unwinding and your screen is doing its pixelated dance moves!",
-        image:"portfolioV1.png",
-        link:"https://thanisha23-portfolio.vercel.app/",
-        repo:"https://github.com/Thanisha23/Portfolio",
-    },
-    {
-        name:"Vawulens",
-        tech_stack:[
-            "/logos/react-2.svg",
-            "/logos/logo-javascript.svg",
-            "/logos/tailwind-css-2.svg",
-        ],
-        status:"Completed",
-        about:"A Gym website crafted using Tailwind and JavaScript for a visually appealing web interface.",
-        image:"vawulens.png",
-        link:"https://vawulens-gym-website.netlify.app/",
-        repo:"https://github.com/Thanisha23/Gym-Website",
-    },
-    
-  
-    {
-    name:"AnyDo:>",
-    tech_stack:[
-        "logos/react-2.svg",
-        "/logos/logo-javascript.svg",
-        "/logos/tailwind-css-2.svg",
-        "/logos/redux.png"
+  {
+    name: "EmailCraft",
+    tech_stack: [
+      {
+        name: "Next.js",
+        badge:
+          "https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white",
+      },
+      {
+        name: "React",
+        badge:
+          "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+      {
+        name: "TypeScript",
+        badge:
+          "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "ReactFlow",
+        badge:
+          "https://img.shields.io/badge/ReactFlow-FF4F59?style=for-the-badge&logo=react&logoColor=white",
+      },
+      {
+        name: "Lucide React",
+        badge:
+          "https://img.shields.io/badge/Lucide_React-434752?style=for-the-badge&logo=feather&logoColor=white",
+      },
+      {
+        name: "Sonner",
+        badge:
+          "https://img.shields.io/badge/Sonner-171717?style=for-the-badge&logo=react&logoColor=white",
+      },
+      {
+        name: "Express.js",
+        badge:
+          "https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white",
+      },
+      {
+        name: "MongoDB",
+        badge:
+          "https://img.shields.io/badge/MongoDB-47A248?style=for-the-badge&logo=mongodb&logoColor=white",
+      },
+      {
+        name: "Agenda.js",
+        badge:
+          "https://img.shields.io/badge/Agenda.js-222222?style=for-the-badge&logo=javascript&logoColor=white",
+      },
+      {
+        name: "JWT",
+        badge:
+          "https://img.shields.io/badge/JWT-000000?style=for-the-badge&logo=json-web-tokens&logoColor=white",
+      },
+      {
+        name: "Nodemailer",
+        badge:
+          "https://img.shields.io/badge/Nodemailer-22B573?style=for-the-badge&logo=node.js&logoColor=white",
+      },
     ],
-    status:"Completed",
-    about:"Built a chic to-do app with React, JavaScript, and Tailwind CSS for visually pleasing task management.",
-    image:"anydo.jpeg",
-    link:"https://todo-quad-b.vercel.app/",
-    repo:"https://github.com/Thanisha23/Todo-QuadB",
-},
+    status: "Completed",
+    about:
+      "A visual platform for creating and managing email campaigns through an intuitive flowchart interface.",
+    image: "emailCraft.png",
+    link: "https://emailcraft.thanisha.tech",
+    repo: "https://github.com/Thanisha23/EmailCraft",
+  },
 
-{
-    name:"Connectify",
-    tech_stack:[
-        "/logos/react-2.svg",
-        "/logos/logo-javascript.svg",
-        "/logos/tailwind-css-2.svg",
+  {
+    name: "EonCapsule",
+    tech_stack: [
+      {
+        name: "Next.js",
+        badge:
+          "https://img.shields.io/badge/Next.js-000?style=for-the-badge&logo=nextdotjs&logoColor=fff",
+      },
+      {
+        name: "React",
+        badge:
+          "https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black",
+      },
+      {
+        name: "TypeScript",
+        badge:
+          "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+      {
+        name: "PostgreSQL",
+        badge:
+          "https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white",
+      },
+      {
+        name: "Prisma",
+        badge:
+          "https://img.shields.io/badge/Prisma-2D3748?style=for-the-badge&logo=prisma&logoColor=white",
+      },
+      {
+        name: "Express",
+        badge:
+          "https://img.shields.io/badge/Express-000000?style=for-the-badge&logo=express&logoColor=white",
+      },
     ],
-    status:"Ongoing",
-    about:"React and Tailwind crafted landing page for your social app with secure authentication.",
-    image:"connectifyy.png",
-    link:"",
-    repo:"",
-},
-
-{
-    name:"Snake-Game",
-    tech_stack:[
-        "/logos/html-icon.png",
-        "/logos/css-icon.png",
-        "/logos/logo-javascript.svg",
-        
+    status: "Ongoing",
+    about:
+      "Virtual Time Capsule lets users store and schedule digital messages, files, and videos for future release.",
+    image: "eoncapsulee.png",
+    link: "https://eoncapsule.thanisha.tech",
+    repo: "",
+  },
+  {
+    name: "Blogosphere",
+    tech_stack: [
+      {
+        name: "React",
+        badge:
+          "https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black",
+      },
+      {
+        name: "TypeScript",
+        badge:
+          "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+      {
+        name: "Hono",
+        badge:
+          "https://img.shields.io/badge/Hono-E36002?style=for-the-badge&logo=hono&logoColor=white",
+      },
+      {
+        name: "PostgreSQL",
+        badge:
+          "https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white",
+      },
+      {
+        name: "Prisma",
+        badge:
+          "https://img.shields.io/badge/Prisma-2D3748?style=for-the-badge&logo=prisma&logoColor=white",
+      },
+      {
+        name: "Cloudflare Workers",
+        badge:
+          "https://img.shields.io/badge/Cloudflare_Workers-F38020?style=for-the-badge&logo=cloudflare&logoColor=white",
+      },
     ],
-    status:"Completed",
-    about:"Built a Snake Game that deepened my understanding of javascript,Have fun playing!",
-    image:"snakegame.jpeg",
-    link:"https://snakerun-game.netlify.app/",
-    repo:"https://github.com/Thanisha23/Snake-Game",
-},
-{
-    name:"SimplyRecipes",
-    tech_stack:[
-        "/logos/html-icon.png",
-        "/logos/css-icon.png",
-        "/logos/logo-javascript.svg",
+    status: "Completed",
+    about:
+      "Create, manage, share content effortlessly with a responsive, user-friendly design.",
+    image: "blogospheree.jpeg",
+    link: "https://blogospheree.vercel.app/",
+    repo: "https://github.com/Thanisha23/Blogosphere",
+  },
+  {
+    name: "PayBuddy",
+    tech_stack: [
+      {
+        name: "React",
+        badge:
+          "https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black",
+      },
+      {
+        name: "TypeScript",
+        badge:
+          "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+      {
+        name: "MongoDB",
+        badge:
+          "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
+      },
+      {
+        name: "Express",
+        badge:
+          "https://img.shields.io/badge/Express-000000?style=for-the-badge&logo=express&logoColor=white",
+      },
+      {
+        name: "Node.js",
+        badge:
+          "https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white",
+      },
     ],
-    status:"Completed",
-    about:"Crafted a recipes site with HTML, CSS, and JS, offering diverse and delightful culinary ideas.",
-    image:"recipes.png",
-    link:"https://thanisha23.github.io/Recipes-Website/",
-    repo:"https://github.com/Thanisha23/Recipes-Website"
-}
-]
+    status: "Completed",
+    about:
+      "A responsive banking application with seamless  UI and transaction capabilities, styled with Tailwind",
+    image: "paybuddy-1.png",
+    link: "https://pay-buddy-lilac.vercel.app/",
+    repo: "https://github.com/Thanisha23/PayBuddy",
+  },
+  {
+    name: "My Portfolio",
+    tech_stack: [
+      {
+        name: "Next.js",
+        badge:
+          "https://img.shields.io/badge/Next.js-000?style=for-the-badge&logo=nextdotjs&logoColor=fff",
+      },
+      {
+        name: "TypeScript",
+        badge:
+          "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+    ],
+    status: "Completed",
+    about:
+      "The very place where your cursor is currently unwinding and your screen is doing its pixelated dance moves!",
+    image: "portfolioV1.png",
+    link: "https://thanisha23-portfolio.vercel.app/",
+    repo: "https://github.com/Thanisha23/Portfolio",
+  },
+  {
+    name: "Vawulens",
+    tech_stack: [
+      {
+        name: "React",
+        badge:
+          "https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black",
+      },
+      {
+        name: "JavaScript",
+        badge:
+          "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+    ],
+    status: "Completed",
+    about:
+      "A Gym website crafted using Tailwind and JavaScript for a visually appealing web interface.",
+    image: "vawulens.png",
+    link: "https://vawulens-gym-website.netlify.app/",
+    repo: "https://github.com/Thanisha23/Gym-Website",
+  },
 
+  {
+    name: "AnyDo:>",
+    tech_stack: [
+      {
+        name: "React",
+        badge:
+          "https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black",
+      },
+      {
+        name: "JavaScript",
+        badge:
+          "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+      {
+        name: "Redux",
+        badge:
+          "https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white",
+      },
+    ],
+    status: "Completed",
+    about:
+      "Built a chic to-do app with React, JavaScript, and Tailwind CSS for visually pleasing task management.",
+    image: "anydo.jpeg",
+    link: "https://todo-quad-b.vercel.app/",
+    repo: "https://github.com/Thanisha23/Todo-QuadB",
+  },
 
-export {items};
+  {
+    name: "Connectify",
+    tech_stack: [
+      {
+        name: "React",
+        badge:
+          "https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black",
+      },
+      {
+        name: "JavaScript",
+        badge:
+          "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+      },
+      {
+        name: "Tailwind CSS",
+        badge:
+          "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+      },
+    ],
+    status: "Ongoing",
+    about:
+      "React and Tailwind crafted landing page for your social app with secure authentication.",
+    image: "connectifyy.png",
+    link: "",
+    repo: "",
+  },
+
+  {
+    name: "Snake-Game",
+    tech_stack: [
+      {
+        name: "HTML",
+        badge:
+          "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
+      },
+      {
+        name: "CSS",
+        badge:
+          "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white",
+      },
+      {
+        name: "JavaScript",
+        badge:
+          "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+      },
+    ],
+    status: "Completed",
+    about:
+      "Built a Snake Game that deepened my understanding of javascript,Have fun playing!",
+    image: "snakegame.jpeg",
+    link: "https://snakerun-game.netlify.app/",
+    repo: "https://github.com/Thanisha23/Snake-Game",
+  },
+  {
+    name: "SimplyRecipes",
+    tech_stack: [
+      {
+        name: "HTML",
+        badge:
+          "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
+      },
+      {
+        name: "CSS",
+        badge:
+          "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white",
+      },
+      {
+        name: "JavaScript",
+        badge:
+          "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+      },
+    ],
+    status: "Completed",
+    about:
+      "Crafted a recipes site with HTML, CSS, and JS, offering diverse and delightful culinary ideas.",
+    image: "recipes.png",
+    link: "https://thanisha23.github.io/Recipes-Website/",
+    repo: "https://github.com/Thanisha23/Recipes-Website",
+  },
+];
+
+export { items };
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -92,7 +92,7 @@ const ProjectsPage = () => {
           {items.map((item) => (
             <motion.div key={item.name} variants={itemVariants}>
               <CardContainer className="inter-var">
-                <CardBody className="relative group/card dark:hover:shadow-2xl dark:hover:shadow-white/[0.1] dark:bg-gray-900/80 bg-gray-900/80 dark:border-white/[0.2] border-white/[0.2] w-[22rem] sm:w-[30rem] h-[29rem] md:h-[29rem] rounded-xl p-6 border backdrop-blur-sm">
+                <CardBody className="relative group/card dark:hover:shadow-2xl dark:hover:shadow-white/[0.1] dark:bg-gray-900/80 bg-gray-900/80 dark:border-white/[0.2] border-white/[0.2] w-[22rem] sm:w-[30rem] h-[29rem] md:h-[29rem] rounded-xl p-6 border backdrop-blur-sm flex flex-col">
                   <CardItem
                     translateZ="50"
                     className="text-2xl font-bold text-white dark:text-white mx-auto"
@@ -103,65 +103,61 @@ const ProjectsPage = () => {
                   <button className="absolute text-[0.7rem] bg-white px-[0.4rem] py-[0.1rem] top-[1rem] right-[1rem] rounded-md text-black">
                     {item.status}
                   </button>
+
                   <CardItem
                     as="p"
                     translateZ="60"
-                    className="md:text-neutral-500 text-neutral-300 md:text-sm text-sm md:max-w-sm max-w-sm md:h-10 h-10 md:overflow-hidden overflow-hidden md:mt-3 mt-3 md:dark:text-neutral-300 dark:text-neutral-300 md:mx-auto mx-auto"
+                    className="md:text-neutral-400 text-neutral-300 md:text-sm text-sm max-w-full md:min-h-[3rem] md:mb-2 mb-2 md:mt-3 mt-3 md:dark:text-neutral-300 dark:text-neutral-300 md:mx-auto mx-auto line-clamp-3"
                   >
                     {item.about}
                   </CardItem>
 
-                  <CardItem translateZ="100" className="w-full mt-4">
+                  <CardItem
+                    translateZ="100"
+                    className="w-full mt-2 flex-shrink-0"
+                  >
                     <Image
                       src={`/${item.image}`}
                       height="1000"
                       width="1000"
-                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                      className="h-[13rem] w-full object-cover rounded-xl group-hover/card:shadow-xl"
                       alt="thumbnail"
                     />
                   </CardItem>
 
                   <CardItem
                     translateZ={20}
-                    as="button"
-                    className="px-4 flex gap-4 justify-center items center py-2 rounded-xl text-xs font-normal text-white dark:text-white md:mt-2 mt-2"
+                    className="mt-2 flex-shrink-0 h-[3.5rem] overflow-hidden"
                   >
-                    {item.tech_stack.map((tech, techIndex) => (
-                      <Image
-                        key={techIndex}
-                        className={
-                          tech.includes("/logos/tailwind-css-2.svg")
-                            ? "pt-1"
-                            : tech.includes("/logos/express-1")
-                            ? "pt-2"
-                            : ""
-                        }
-                        src={tech}
-                        width={
-                          tech.includes("/logos/hono-1.png")
-                            ? 20
-                            : tech.includes("/logos/express-1")
-                            ? 70
-                            : 25
-                        }
-                        height={25}
-                        alt="Picture of the author"
-                      />
-                    ))}
+                    <div className="flex flex-wrap gap-2 justify-center items-center px-1 overflow-y-auto max-h-[3.5rem] scrollbar-container">
+                      {Array.isArray(item.tech_stack) &&
+                        item.tech_stack[0]?.name &&
+                        item.tech_stack.map((tech, techIndex) => (
+                          <img
+                            key={techIndex}
+                            src={tech.badge}
+                            alt={tech.name}
+                            height="25"
+                            className="h-6"
+                          />
+                        ))}
+                    </div>
                   </CardItem>
 
-                  <div className="md:flex flex md:justify-between justify-between md:items-center items center">
+                  <div className="mt-auto pt-2 flex justify-between items-center w-full">
                     <CardItem
                       translateZ={20}
                       as="button"
                       className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white text-white"
                     >
-                      <a href={item.link}>{item.link === "" ? "Coming soon..": "Check Live →"}</a>
+                      <a href={item.link}>
+                        {item.link === "" ? "Coming soon.." : "Check Live →"}
+                      </a>
                     </CardItem>
                     <CardItem
                       translateZ={20}
                       as="button"
-                      className="px-2 py-2 rounded-3xl bg-black dark:bg-white mb-3 dark:text-black text-white text-xs font-bold"
+                      className="px-2 py-2 rounded-3xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                     >
                       <a href={item.repo}>
                         <FaGithub size={20} />
